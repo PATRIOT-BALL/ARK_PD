a1942bc1a0d562b73c9248463f2127cac982a30d
PATRIOT-BALL <gkdlsptmxm@naver.com>
2021-05-10 22:42:50 +0900

    임프 수정
    비싸다니! 이 쓰레기들을 모으느라 얼마나 고생했는데.

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ImpSprite.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ImpSprite.java
@@ -34,18 +34,17 @@ public class ImpSprite extends MobSprite {
 		
 		texture( Assets.Sprites.IMP );
 		
-		TextureFilm frames = new TextureFilm( texture, 12, 14 );
+		TextureFilm frames = new TextureFilm( texture, 32, 38 );
 		
-		idle = new Animation( 10, true );
+		idle = new Animation( 8, true );
 		idle.frames( frames,
-			0, 1, 2, 3, 0, 1, 2, 3, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-			0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 3, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4 );
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 );
 		
 		run = new Animation( 20, true );
 		run.frames( frames, 0 );
 		
 		die = new Animation( 10, false );
-		die.frames( frames, 0, 3, 2, 1, 0, 3, 2, 1, 0 );
+		die.frames( frames, 14, 15, 16, 17, 18, 19, 20, 21, 22, 22 );
 		
 		play( idle );
 	}
