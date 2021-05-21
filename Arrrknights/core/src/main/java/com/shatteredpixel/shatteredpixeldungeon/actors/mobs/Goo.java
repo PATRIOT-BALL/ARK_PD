2a735a85a5de30037f310e8cf3a7861e3fec2700
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-05-22 01:03:37 +0900

    수정

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Goo.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Goo.java
@@ -237,7 +237,10 @@ public class Goo extends Mob {
 				ofs = PathFinder.NEIGHBOURS8[Random.Int(8)];
 			} while (!Dungeon.level.passable[pos + ofs]);
 			Dungeon.level.drop( new GooBlob(), pos + ofs ).sprite.drop( pos );
-			Dungeon.level.drop( new ThrowingClub(), pos + ofs).sprite.drop(pos);
+		}
+		for (int i = 0; i < 2; i++) {
+			int ofs = PathFinder.NEIGHBOURS8[Random.Int(8)];
+			Dungeon.level.drop(new ThrowingClub(), pos + ofs).sprite.drop(pos);
 		}
 		
 		Badges.validateBossSlain();
