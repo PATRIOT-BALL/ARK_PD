e8401cd172289aeddf3003973a790cf6b7de845a
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-06-11 01:21:35 +0900

    버전처리

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
@@ -281,7 +281,7 @@ public class TitleScene extends PixelScene {
 		@Override
 		protected void onClick() {
 			TomorrowRogueNight.scene().addToFront(new WndOptions(
-					Messages.get(this, "versioned_title") + "ver0.1.3",
+					Messages.get(this, "versioned_title") + "ver0.1.5",
 					Messages.get(this, "desc"),
 					Messages.get(this, "update"),
 					Messages.get(this, "changes")
