ea62aec7a0b9a011b0368c4a6f1d6bd5533fead7
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-06-27 20:23:30 +0900

    서리별 무기

---
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WintersScar.java
@@ -0,0 +1,57 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Chill;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.Random;
+
+public class WintersScar extends MeleeWeapon {
+    {
+        image = ItemSpriteSheet.WAR_HAMMER;
+        hitSound = Assets.Sounds.HIT_CRUSH;
+        hitSoundPitch = 1.12f;
+
+        tier = 5;
+    }
+
+    @Override
+    public int max(int lvl) {
+        return  4*(tier) +    //20 base
+                lvl*(tier);
+    }
+
+    @Override
+    public int proc(Char attacker, Char defender, int damage) {
+        if (defender.buff(Chill.class) != null) damage = Math.round(damage * 1.3f);
+
+        if (defender instanceof Mob && ((Mob) defender).surprisedBy(attacker)) {
+            Buff.affect(defender, Chill.class, 4f);
+        }
+        return super.proc(attacker, defender, damage);
+    }
+
+    @Override
+    public int damageRoll(Char owner) {
+        if (owner instanceof Hero) {
+            Hero hero = (Hero)owner;
+            Char enemy = hero.enemy();
+            if (enemy instanceof Mob && ((Mob) enemy).surprisedBy(hero)) {
+                //deals 33% toward max to max on surprise, instead of min to max.
+                int diff = max() - min();
+                int damage = augment.damageFactor(Random.NormalIntRange(
+                        min() + Math.round(diff*0.33f),
+                        max()));
+                int exStr = hero.STR() - STRReq();
+                if (exStr > 0) {
+                    damage += Random.IntRange(0, exStr);
+                }
+                return damage;
+            }
+        }
+        return super.damageRoll(owner);
+    }
+}
