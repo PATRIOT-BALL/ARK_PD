13bf33cc72f8e7abfb8397a08045e558844f2e6c
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-07-09 18:18:26 +0900

    침묵식칼

---
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/Kikitchenknife.java
@@ -0,0 +1,26 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Silence;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class Kikitchenknife extends MissileWeapon {
+    {
+        image = ItemSpriteSheet.TENGU_SHOCKER;
+        hitSound = Assets.Sounds.LIGHTNING;
+        hitSoundPitch = 0.8f;
+
+        tier = 3;
+        baseUses = 8;
+        sticky = false;
+    }
+
+    @Override
+    public int proc(Char attacker, Char defender, int damage) {
+        Buff.affect(defender, Silence.class, 10f);
+
+        return super.proc(attacker, defender, damage);
+    }
+}
