381896bba831d0ab1a1242c0a66ad92f4ef5ac60
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-10-01 00:55:35 +0900

    오리지뉴턴트 챔피언 못걸리게

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/Level.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/Level.java
@@ -48,6 +48,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Bestiary;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mimic;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Originiutant;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.YogFist;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Lens;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Sheep;
@@ -469,7 +470,7 @@ public abstract class Level implements Bundlable {
 		}
 
 		Mob m = Reflection.newInstance(mobsToSpawn.remove(0));
-		if (Dungeon.isChallenged(Challenges.CHAMPION_ENEMIES)){
+		if (Dungeon.isChallenged(Challenges.CHAMPION_ENEMIES) && !(m instanceof Originiutant)){
 			ChampionEnemy.rollForChampion(m);
 		}
 		return m;
