bd061a141a0b32c13a0c743c98adf361ddf6a481
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-10-01 22:20:12 +0900

    몹 하향

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Eye.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Eye.java
@@ -48,7 +48,7 @@ public class Eye extends Mob {
 	{
 		spriteClass = A_master2Sprite.class;
 		
-		HP = HT = 100;
+		HP = HT = 90;
 		defenseSkill = 12;
 		viewDistance = Light.DISTANCE;
 		
@@ -185,7 +185,7 @@ public class Eye extends Mob {
 			}
 
 			if (hit( this, ch, true )) {
-				ch.damage( Random.NormalIntRange( 35, 55 ), new DeathGaze() );
+				ch.damage( Random.NormalIntRange( 30, 55 ), new DeathGaze() );
 
 				if (Dungeon.level.heroFOV[pos]) {
 					ch.sprite.flash();
