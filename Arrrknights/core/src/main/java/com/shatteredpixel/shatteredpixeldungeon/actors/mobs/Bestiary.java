d9be751bece8d5f9a746865b348180de1919b11c
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-10-01 23:43:23 +0900

    Update Bestiary.java

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Bestiary.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Bestiary.java
@@ -268,6 +268,11 @@ public class Bestiary {
 			case 19:
 				if (Random.Float() < 0.025f) rotation.add(Succubus.class);
 				return;
+
+			case 34:
+				// Siest 1
+				if (Random.Float() < 0.025f) rotation.add(Rockbreaker.class);
+				return;
 		}
 	}
 	
