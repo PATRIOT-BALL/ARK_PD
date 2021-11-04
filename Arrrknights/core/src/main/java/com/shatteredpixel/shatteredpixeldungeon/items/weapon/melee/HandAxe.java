f9094079573b7c6905799cb6e2cb23a289ba012a
PATRIOT-BALL <gkdlsptmxm@naver.com>
2021-11-05 00:09:33 +0900

    효과음 롤백

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/HandAxe.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/HandAxe.java
@@ -28,7 +28,7 @@ public class HandAxe extends MeleeWeapon {
 
 	{
 		image = ItemSpriteSheet.HAND_AXE;
-		hitSound = Assets.Sounds.HIT_STRIKE;
+		hitSound = Assets.Sounds.HIT_BONK;
 		hitSoundPitch = 1f;
 
 		tier = 2;
