810d802040e5e6f6830e642209bb65467687a234
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-11-06 06:02:10 +0900

    잡수

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/RadiantSpear.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/RadiantSpear.java
@@ -3,6 +3,8 @@ package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Blindness;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.RadiantKnight;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Vulnerable;
 import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.TalismanOfForesight;
@@ -11,6 +13,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfSunLight;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfTenacity;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.Random;
 
 public class RadiantSpear extends MeleeWeapon {
     {
@@ -19,7 +22,7 @@ public class RadiantSpear extends MeleeWeapon {
         hitSoundPitch = 1.18f;
 
         tier = 5;
-        ACC = 1.25f;
+        ACC = 1.15f;
     }
 
     @Override
@@ -30,6 +33,9 @@ public class RadiantSpear extends MeleeWeapon {
 
     @Override
     public int proc(Char attacker, Char defender, int damage) {
+        if (defender.buff(Blindness.class) != null) {
+            Buff.affect(defender, Vulnerable.class, 3f); }
+
         if (defender.buff(Vulnerable.class) != null) {
             if (Dungeon.hero.belongings.getItem(RingOfSunLight.class) != null && Dungeon.hero.belongings.getItem(RingOfHaste.class) != null) {
                 if (Dungeon.hero.belongings.getItem(RingOfSunLight.class).isEquipped(Dungeon.hero) && Dungeon.hero.belongings.getItem(RingOfHaste.class).isEquipped(Dungeon.hero)) {
