228bb0e55da19668d73d645d8b74594aed05762b
lamw125n <83650660+lamw125n@users.noreply.github.com>
2021-11-29 00:01:31 +0900

    처리

---
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Gunaccessories/GunScope.java
@@ -0,0 +1,13 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.Gunaccessories;
+
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class GunScope extends Accessories {
+    {
+        image = ItemSpriteSheet.ARMOR_CLOTH;
+
+        ACCcorrectionvalue = 1.3f;
+        DLYcorrectionvalue = 1.5f;
+        DMGcorrectionvalue = 1.2f;
+    }
+}
