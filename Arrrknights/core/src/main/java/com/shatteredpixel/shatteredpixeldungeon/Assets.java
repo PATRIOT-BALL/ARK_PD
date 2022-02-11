986998803f4f757616d4e027b044042090b076b8
PATRIOT-BALL <gkdlsptmxm@naver.com>
2022-02-11 23:17:02 +0900

    리스캄 사전작업

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -315,6 +315,7 @@ public class Assets {
 		public static final String GREY	= "sprites/greythroat.png";
 		public static final String ROSEMARY	= "sprites/rosemary.png";
 		public static final String NEARL	= "sprites/nearl.png";
+		public static final String RISKARM	= "sprites/riskarm.png";
 		public static final String AVATARS	= "sprites/avatars.png";
 		public static final String PET		= "sprites/pet.png";
 		public static final String DONUT		= "sprites/Wraith_donut.png";
