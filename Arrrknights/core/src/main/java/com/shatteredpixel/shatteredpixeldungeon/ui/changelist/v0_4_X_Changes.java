5d05788a023cba788dac2b6baa247ffbf7692b9c
lamw125n <83650660+lamw125n@users.noreply.github.com>
2022-04-09 15:42:55 +0900

    Update v0_4_X_Changes.java

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v0_4_X_Changes.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v0_4_X_Changes.java
@@ -50,6 +50,9 @@ public class v0_4_X_Changes {
         changes.hardlight(CharSprite.NEGATIVE);
         changeInfos.add(changes);
 
+        changes.addButton(new ChangeButton(Icons.get(Icons.WEP), "무기 너프",
+                "_고난의 진술자_의 명중률 보정치가 0%에서 _-20%_로 감소했으며, 더 이상 기습공격을 할 수 없습니다."));
+
 
     }
 
