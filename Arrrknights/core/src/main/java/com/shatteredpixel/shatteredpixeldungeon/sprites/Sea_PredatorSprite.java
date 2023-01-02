826c2509392c906eb1e55a5c792faf0eb11f035e
PATRIOT-BALL <gkdlsptmxm@naver.com>
2023-01-02 20:52:45 +0900

    ddd

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/Sea_PredatorSprite.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/Sea_PredatorSprite.java
@@ -37,13 +37,13 @@ public class Sea_PredatorSprite extends MobSprite {
         idle.frames( frames, 0 );
 
         run = new Animation( 12, true );
-        run.frames( frames, 0 );
+        run.frames( frames, 1, 2, 3, 4, 5, 6 );
 
         attack = new Animation( 15, false );
-        attack.frames( frames, 0 );
+        attack.frames( frames, 7, 8, 9, 10, 11, 12, 13 );
 
         die = new Animation( 10, false );
-        die.frames( frames, 0 );
+        die.frames( frames, 14, 15, 16, 17, 18, 19, 20 );
 
         play( idle );
     }
