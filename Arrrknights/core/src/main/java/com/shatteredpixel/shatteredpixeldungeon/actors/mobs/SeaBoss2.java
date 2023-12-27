b088074599116d5723be580b183a9142b71f68ad
lamw125n <83650660+lamw125n@users.noreply.github.com>
2023-12-27 09:27:07 +0900

    Update SeaBoss2.java

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SeaBoss2.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SeaBoss2.java
@@ -1,11 +1,16 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Corruption;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Drowsy;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.MagicalSleep;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Silence;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Sleep;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTeleportation;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfBlastWave;
+import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.MephistoSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.Skadi_mulaSprite;
 import com.watabou.utils.Random;
@@ -14,17 +19,21 @@ public class SeaBoss2 extends Mob {
     {
         spriteClass = Skadi_mulaSprite.class;
 
-        HP = HT = 1000;
+        HP = HT = 1500;
         defenseSkill = 2;
 
         properties.add(Property.BOSS);
         immunities.add(Sleep.class);
     }
 
+    int summoncooldown = 0;
+
     @Override
     protected boolean act() {
         return super.act();
     }
+
+    // summon pos = 50, 54
 
     @Override
     public int attackSkill(Char target) {
@@ -33,12 +42,36 @@ public class SeaBoss2 extends Mob {
 
     private void SummonEnemy()
     {
+        Mob summonEnemy1;
+        int summonpos1 = 50;
+        if (Random.Int(4) != 0)
+            summonEnemy1 = new SummonRunner();
+        else
+            summonEnemy1 = new SummonOcto();
+
+        Mob summonEnemy2;
+        int summonpos2 = 54;
+        if (Random.Int(4) != 0)
+            summonEnemy2 = new SummonRunner();
+        else
+            summonEnemy2 = new SummonLeef();
+
+
+        summonEnemy1.pos = summonpos1;
+        GameScene.add(summonEnemy1, 1f);
+        if (summonpos1 == Dungeon.hero.pos)  ScrollOfTeleportation.teleportChar_unobstructed(summonEnemy1);
+
+        summonEnemy2.pos = summonpos2;
+        GameScene.add(summonEnemy2, 1f);
+        if (summonpos2 == Dungeon.hero.pos)  ScrollOfTeleportation.teleportChar_unobstructed(summonEnemy2);
+
+        summoncooldown = 5;
 
     }
 
     // 보스 소환물 (3종) 한번에 2마리 소환
-    // 기본 소환은 런너 (확률 4/6)
-    // 각각 1/6확률로 Octo, Leaf가 소환될 수 있음
+    // 기본 소환은 런너 (확률 75%)
+    // 각각 25%확률로 Octo, Leaf가 소환될 수 있음
     public static class SummonRunner extends SeaRunner {
         {
             state = HUNTING;
